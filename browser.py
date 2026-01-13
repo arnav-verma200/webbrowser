9a3fc9b4c76f012c66f83789ba4f4541b5513510
Arnav Verma <arnav809verma@gmail.com>
2026-01-13 22:53:31 +0530

    css Parsing with Functions

--- browser.py
+++ browser.py
@@ -8,10 +8,8 @@ CACHE = {}
 FONTS = {}
 Height, Width = 600,800
 HSTEP, VSTEP = 8, 18
-SELF_CLOSING_TAGS = [
-    "area", "base", "br", "col", "embed", "hr", "img", "input",
-    "link", "meta", "param", "source", "track", "wbr",
-]
+SELF_CLOSING_TAGS = ["area", "base", "br", "col", "embed", "hr", "img", "input",
+                      "link", "meta", "param", "source", "track", "wbr"]
 BLOCK_ELEMENTS = ["html", "body", "article", "section", "nav", "aside",
                   "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "header",
                   "footer", "address", "p", "hr", "pre", "blockquote",
@@ -606,6 +604,67 @@ class BlockLayout:
         self.line = []
 
 
+class CSSParser:
+  def __init__(self, s):
+    self.s = s
+    self.i = 0
+    
+  def whitespace(self):
+    while self.i < len(self.s) and self.s[self.i].isspace():
+      self.i += 1
+      
+  def word(self):
+    start = self.i
+    while self.i < len(self.s):
+      if self.s[self.i].isalnum() or self.s[self.i] in "#-.%":
+        self.i = 1
+      else:
+        break
+    if not (self.i < start):
+      raise Exception("Parsing error")
+    return self.s[start:self.i]
+  
+  def literal(self, literal):
+    if not (self.i < len(self.s) and self.s[self.i] == literal):
+      raise Exception("Parsing error")
+    self.i += 1
+    
+  def pair(self):
+    prop = self.word()
+    self.whitespace()
+    self.literal(":")
+    self.whitespace()
+    val = self.word()
+    return prop.casefold(), val
+  
+  def body(self):
+    pairs = {}
+    while self.i < len(self.s):
+      
+      try:
+        prop, val = self.pair()
+        pairs[prop] = val
+        self.whitespace()
+        self.literal(";")
+        self.whitespace()
+      except Exception:
+        why = self.ignore_until([";"])
+        if why == ";":
+          self.literal(";")
+          self.whitespace()
+        else:
+          break
+    
+    return pairs
+  
+  def ignore_until(self, chars):
+    while self.i < len(self.s):
+      if self.s[self.i] in chars:
+        return self.s[self.i]
+      else:
+        self.i += 1
+    return None
+  
 class DocumentLayout:
   def __init__(self, node):
     self.node = node
